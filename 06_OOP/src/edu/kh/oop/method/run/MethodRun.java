5820579380ca974e8c3c1068a916a9e98e2a0f3d
JYkim25 <ganel20k@gmail.com>
2025-02-21 17:49:39 +0900

    250221

---
+++ 06_OOP/src/edu/kh/oop/method/run/MethodRun.java
@@ -0,0 +1,28 @@
+package edu.kh.oop.method.run;
+
+
+import edu.kh.oop.method.model.service.MemberService;
+import edu.kh.oop.method.model.service.MethodExample;
+
+public class MethodRun {
+
+	public static void main(String[] args) {
+
+		MethodExample methodEx = new MethodExample();
+		methodEx.method1();
+		
+		MemberService service = new MemberService();
+		service.displayMenu();
+
+		// |					   |
+		// | method2() 메서드 호출 |
+		// | method1() 메서드 실행 |
+		// | main 메서드 실행	   |
+		//  -----------------------
+		//			Stack
+		// 쌓인 반대 순서로 빠짐
+		
+		
+	}
+
+}
